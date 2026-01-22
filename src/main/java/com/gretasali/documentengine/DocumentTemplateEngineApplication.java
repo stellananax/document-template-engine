6a4137876f295174fb8551b49924dee8fc199b5d
gretasali <gretasali25@gmail.com>
2026-01-22 09:22:24 +0100

    chore: initial Spring Boot setup with Dockerized Postgres

---
+++ src/main/java/com/gretasali/documentengine/DocumentTemplateEngineApplication.java
@@ -0,0 +1,13 @@
+package com.gretasali.documentengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentTemplateEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentTemplateEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gretasali/documentengine/DocumentTemplateEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gretasali.documentengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentTemplateEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
